e16455e877a446592cc5a039539bffcbf183a6b2
ayoubalami <ayoubalami6@gmail.com>
2023-02-01 10:13:08 +0000

    fix merge conflict back to master

--- servo_motor.py
+++ servo_motor.py
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-
-import pigpio
-import time
-
-class ServoMotor:    
-
-    def __init__(self,servo_pin):
-        self.pi = pigpio.pi()
-        self.servo_pin = servo_pin
-        self.min_pulse=660
-        self.max_pulse=2400 
-
-    def goToAngleWithSpeed(self,angle,speed):
-        angle=angle+90
-        target_pulse_width=round(((self.max_pulse-self.min_pulse)*angle)/180)+self.min_pulse
-        print(target_pulse_width)
-        current_pulse_width = self.pi.get_servo_pulsewidth(self.servo_pin)
-        print("Current pulse width: ", current_pulse_width)
-        while(current_pulse_width!=target_pulse_width):
-            if (current_pulse_width>target_pulse_width):
-                current_pulse_width-=1
-            else:
-                current_pulse_width+=1
-            self.pi.set_servo_pulsewidth(self.servo_pin, current_pulse_width)
-            time.sleep(speed)
-        print("Done")
- 
-    def goToAngle(self,angle):
-        angle=angle+90
-        pulse_width=(((self.max_pulse-self.min_pulse)*angle)/180)+self.min_pulse
-        print(pulse_width)
-        self.pi.set_servo_pulsewidth(self.servo_pin, pulse_width)
-        
-# motor=ServoMotor(servo_pin=23)
-# # motor=ServoMotor(servo_pin=18) 
-# motor.goToAngle(angle=180) 
-# # time.sleep(2)
-# motor.goToAngle(angle=0) 
-# time.sleep(1)
-
-=======
 
 import pigpio
 import time
@@ -139,4 +97,3 @@ class ServoMotor:
 # motor.goToAngle(angle=0) 
 # time.sleep(1)
 
->>>>>>> b5282ad601f2ee2e5ae964b2523e003a5838db72
--- utils_lib/utils_functions.py
+++ utils_lib/utils_functions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import base64
 from csv import writer
@@ -20,32 +19,3 @@ def runcmd(cmd, verbose = False, *args, **kwargs):
     pass
 
 
-def addTrackingAndDetectionTimeAndFPS(img,detection_time,tracking_time,tracking_fps):
-    width=img.shape[1]
-    cv2.rectangle(img,(int(width-195),10),(int(width-10),85),color=(240,240,240),thickness=-1)
-    cv2.putText(img, f'FPS: {round(tracking_fps,2)}', (int(width-190),30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (25,25,250), 2)
-    cv2.putText(img, f'Det. time: {round(detection_time*1000)}ms', (int(width-190),52), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (250,25,25), 2)
-    cv2.putText(img, f'Tra. time: {round(tracking_time*1000)}ms', (int(width-190),73), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (250,25,25), 2)
-=======
-
-import base64
-from csv import writer
-import subprocess
-import cv2
-from utils_lib.enums import ClientStreamTypeEnum
-import numpy as np
-def runcmd(cmd, verbose = False, *args, **kwargs):
-    process = subprocess.Popen(
-        cmd,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE,
-        text = True,
-        shell = True
-    )
-    std_out, std_err = process.communicate()
-    if verbose:
-        print(std_out.strip(), std_err)
-    pass
-
-
->>>>>>> b5282ad601f2ee2e5ae964b2523e003a5838db72
